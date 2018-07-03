98b34450f4f57f7a56283f47d6ea58fc348d2a8e
David Zollikofer <david@zollikofer.co>
2018-07-03 18:00:13 +0200

    added basic args parsing

--- src/main.go
+++ src/main.go
@@ -1,7 +1,113 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	app = kingpin.New("GitBuddy", "A simple program simplifying interacting with Git.")
+	// debug    = app.Flag("debug", "Enable debug mode.").Bool()
+	// serverIP = app.Flag("server", "Server address.").Default("127.0.0.1").IP()
+
+	stage = app.Command("stage", "Stage changes.")
+	st    = app.Command("st", "Stage changes.")
+
+	commit        = app.Command("commit", "Commit changes")
+	commitMessage = commit.Arg("message", "The message for this commit.").Required().String()
+
+	post        = app.Command("post", "Post a message to a channel.")
+	postImage   = post.Flag("image", "Image to post.").File()
+	postChannel = post.Arg("channel", "Channel to post to.").Required().String()
+	postText    = post.Arg("text", "Text to post.").Strings()
+)
 
 func main() {
-	fmt.Print("test")
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+
+	// stage changes
+	case stage.FullCommand():
+		suc := StageChanges()
+		if suc != true {
+			fmt.Println("asd")
+		}
+
+	case st.FullCommand():
+		StageChanges()
+
+	// commit
+	case commit.FullCommand():
+		fmt.Println(*commitMessage)
+
+	case post.FullCommand():
+		if *postImage != nil {
+		}
+		text := strings.Join(*postText, " ")
+		println("Post:", text)
+	}
+}
+
+func mainW() {
+	var (
+		cmdOut []byte
+		err    error
+	)
+	cmdName := "git"
+	cmdArgs := []string{"rev-parse", "--verify", "HEAD"}
+	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "There was an error running git rev-parse command: ", err)
+		os.Exit(1)
+	}
+	sha := string(cmdOut)
+	firstSix := sha[:6]
+	fmt.Println("The first six chars of the SHA at HEAD in this repo are", firstSix)
+}
+
+// StageChanges stages all changes and returns wheter it what sucessful.
+func StageChanges() bool {
+	// var cmdOut []byte
+	success := false
+	var err error
+
+	cmdArgs := []string{"add ."}
+	if _, err = exec.Command("git", cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "There was an error staging all changes", err)
+		return success
+	}
+	success = true
+	return success
+}
+
+// PushRepo pushes to remote
+func PushRepo() bool {
+	// var cmdOut []byte
+	success := false
+	var err error
+
+	cmdArgs := []string{"push"}
+	if _, err = exec.Command("git", cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "There was an error staging all changes", err)
+		return success
+	}
+	success = true
+	return success
+}
+
+// CommitWithMessage commits staged changes with a message.
+func CommitWithMessage() bool {
+	// var cmdOut []byte
+	success := false
+	var err error
+
+	cmdArgs := []string{"push"}
+	if _, err = exec.Command("git", cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "There was an error staging all changes", err)
+		return success
+	}
+	success = true
+	return success
 }
